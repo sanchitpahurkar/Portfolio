6f0e4939a817943a752a360bb61f06bdc0cb4cd8
sanchitpahurkar <pahurkarsm@rknec.edu>
2024-06-08 08:59:40 +0530

    added navbar

---
+++ src/script.js
@@ -0,0 +1,5 @@
+// hamburger menu js
+function Menu(e){
+    let list = document.getElementById('nav-menu');
+    e.name === 'menu' ? (e.name = "close",list.classList.add('top-[80px]') , list.classList.add('opacity-100')) :( e.name = "menu" ,list.classList.remove('top-[80px]'),list.classList.remove('opacity-100'))
+  }
